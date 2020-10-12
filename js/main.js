87b7e406c6b04ab9a5d616a790bdd6bf0d18d958
SkyOverZzz <skyoverlear@rambler.ru>
2020-10-12 18:14:09 +0300

    g

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(".navbar a").click(function(){
+  $("body,html").animate({
+   scrollTop:$("#" + $(this).data('value')).offset().top
+  },1000)
+  
+ })
+jQuery(document).ready(function($) {
+ 	let text = []
+  if ($(window).width() < 1185) {
+ 	$('img').foreach().hasClass('offset-5').removeClass
+  } 
+
+ 
+});
